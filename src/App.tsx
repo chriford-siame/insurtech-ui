f4b69abd8c4e5929c0ce03a34048dd76e85ea61b
💵 Chriford <143299729+chriford-siame@users.noreply.github.com>
2025-04-17 10:46:17 +0200

    Shadcn configurations (#2)
    * Customize tailwind & import aliases
    
    * Add dependencies
    
    * update class values
    
    * Add shadcn css stylesheet
    
    * install button component
    
    * modify app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Button } from '@/components/ui/button';
 
 function App() {
   return (
     <div className="App text-2xl">
       <p className='text-blue-600'>hello</p>
+
+      <Button value={'hello'} onClick={() => {}}/>
     </div>
   );
 }
