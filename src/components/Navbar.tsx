a4465b2e011466016be04255ea98c0f188e4ecc6
chriford-siame <chrifordsiame@gmail.com>
2025-07-21 11:52:46 +0200

    update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,15 @@ function Navbar() {
   const { isAuthenticated, handleLogout } = useAuth();
   return (
     <div className='bg-blue-400 py-4 flex items-center justify-between px-3'>
-      <a href="/" className='text-[18pt] font-bold text-white'><span className='text-orange-500'>I</span><span className='text-gray-200'>Claim</span><span className='text-purple-500'>.</span></a>
+      <a href="/" className='text-[18pt] font-bold text-white'><span className='text-orange-500'>I</span><span className='text-gray-200'>Tech</span><span className='text-purple-500'>.</span></a>
+
+      {isAuthenticated ? 
+      <div className="flex gap-5">
+        <a href="/login" className='text-white rounded-md'>claims</a>
+        <a href="/login" className='text-white rounded-md'>quotaions</a>
+        <a href="/login" className='text-white rounded-md'>policies</a>
+      </div>
+      : null}
 
       {!isAuthenticated ?
         <div className='flex  gap-1 text-[12pt]'>
@@ -13,12 +21,14 @@ function Navbar() {
           <a href="/register" className='text-white bg-gray-600 hover:bg-gray-600 px-2 rounded-md'>Signup</a>
         </div>
         :
-        <div className='text-[12pt]'>
-          <button onClick={() => {
-            handleLogout();
-            window.location.href = "/";
-          }}  className='text-white bg-gray-600 hover:bg-gray-600 px-2 rounded-md'>logout</button>
-        </div>
+        <span>
+          <div className='text-[12pt]'>
+            <button onClick={() => {
+              handleLogout();
+              window.location.href = "/";
+            }}  className='text-white bg-gray-600 hover:bg-gray-600 px-2 rounded-md'>logout</button>
+          </div>
+        </span>
       }
     </div>
   )
