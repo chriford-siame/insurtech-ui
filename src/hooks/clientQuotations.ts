48a392533057bfbd3da426a0876a9b9783a592f8
chriford-siame <chrifordsiame@gmail.com>
2025-07-23 17:19:54 +0200

    modify client quotations hook

--- src/hooks/clientQuotations.ts
+++ src/hooks/clientQuotations.ts
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { ICustomClaim } from "src/interfaces/user";
+import { IMotorInsurance } from "src/interfaces/quotation";
 
 const useClientQuotations = () => {
-  const [quotations, setQuotations] = useState<any[] | []>([]);
+  const [quotations, setQuotations] = useState<IMotorInsurance[] | []>([]);
+  const [clientQuotations, setClientQuotations] = useState<IMotorInsurance[] | []>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
@@ -11,8 +12,8 @@ const useClientQuotations = () => {
     const fetchQuotations = async () => {
       const token = localStorage.getItem('access_token');
       try {
-        const response = await axios.get<ICustomClaim[]>(
-          `http://localhost:8000/quotation/list/`,
+        const response = await axios.get<IMotorInsurance[]>(
+          `http://localhost:8000/quotation/`,
           {
             headers: {
               Authorization: `Bearer ${token}`,
@@ -22,15 +23,35 @@ const useClientQuotations = () => {
         );
         setQuotations(response.data);
       } catch (err) {
-        setError("Failed to fetch user quotations.");
+        setError("Failed to fetch client quotations.");
       } finally {
         setLoading(false);
       }
     }
+    const fetchAllQuotations = async () => {
+      const token = localStorage.getItem('access_token');
+      try {
+        const response = await axios.get<IMotorInsurance[]>(
+          `http://localhost:8000/quotation/list/`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              'Content-Type': 'multipart/form-data',
+            },
+          }
+        );
+        setClientQuotations(response.data);
+      } catch (err) {
+        setError("Failed to fetch all client quotations.");
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchAllQuotations();
     fetchQuotations();
   }, []);
 
-  return { quotations, loading, error };
+  return { quotations, clientQuotations, loading, error };
 };
 
 export default useClientQuotations;
